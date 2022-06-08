84b9e8db684d0f89994acd7fe771d5e6ba5edda7
anjali252 <agrawalanjali65@gmail.com>
2022-06-08 13:08:26 +0530

    New files & modified some files

---
+++ src/test/java/com/seleniumcli/TestClass1.java
@@ -0,0 +1,44 @@
+package com.seleniumcli;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	@BeforeMethod
+	@BeforeClass
+	public void setUp() {
+		System.out.println("\nTestClass1 -> This method runs before class");
+	}
+
+	@AfterClass
+	public void cleanUp() {
+		System.out.println("\nTestClass1 -> This method runs after class");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("\nTestClass1 -> This method runs before each methods");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("\nTestClass1 -> This method runs after each methods");
+	}
+
+	@Test
+	public void testMethod1() {
+		System.out.println("\nRunning test -> TestClass1 -> testMethod1");
+	}
+
+	@Test
+	public void testMethod2() {
+		System.out.println("\nRunning test -> TestClass1 -> testMethod2");
+	}
+
+}
